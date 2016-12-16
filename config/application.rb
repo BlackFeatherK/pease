ea466a0f724e682d38622348163a3b31c0649b32
Brian Wong <brian.wongst@gmail.com>
2016-12-16 19:12:52 +0800

    Create form and add i18n

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,12 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Pease
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.i18n.default_locale = "zh-TW"
   end
 end
