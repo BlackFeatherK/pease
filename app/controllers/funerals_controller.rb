463b0ce8bb5ebffc88301f060814f3b2636ed44e
Brian Wong <brian.wongst@gmail.com>
2016-12-21 22:21:04 +0800

    Adjust funeral page css

--- app/controllers/funerals_controller.rb
+++ app/controllers/funerals_controller.rb
@@ -1,11 +1,26 @@
 class FuneralsController < ApplicationController
-	before_action :find_will
+	before_action :find_will, 
+	
 	def show
 	 @funeral = current_will.funerals.build
+	end
+
+	def create
+		@funeral = @will.funerals.build(funeral_params)
+		if @funeral.save
+			flash[:notice] = 'successful'
+		else 
+			render "show"
+			flash[:alert] = "Fail"	
+		end
 	end
 
 	private
 	def find_will 
 		@will = current_will
+	end
+
+	def funeral_params
+		params.require(:funeral).permit(:preference, :body_arrangement, :burial_arrangement, :funeral_service_ids => [])
 	end
 end
--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -1,4 +1,4 @@
 class Funeral < ApplicationRecord
   belongs_to :will , :optional => true
-  has_many :funeral_services
+  has_and_belongs_to_many :funeral_services
 end
