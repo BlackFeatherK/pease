92bfe80ce3c8dc372f6e5208c8f9c820ce478d39
BlackFeather <she3354248@yahoo.com.tw>
2016-12-22 15:22:57 +0800

    edit partial

--- app/controllers/wills_controller.rb
+++ app/controllers/wills_controller.rb
@@ -7,15 +7,15 @@ class WillsController < ApplicationController
   def show 
     @will = current_will
     @property = current_will.properties.build
-    @properties = current_will.properties
+    @properties = current_will.properties.where.not(:id => nil )
     @stock_portfolio = current_will.stock_portfolios.build
-    @stock_portfolios = current_will.stock_portfolios
+    @stock_portfolios = current_will.stock_portfolios.where.not(:id => nil)
     @account = current_will.accounts.build
-    @accounts = current_will.accounts
+    @accounts = current_will.accounts.where.not(:id => nil)
     @motor = current_will.motors.build
-    @motors = current_will.motors
+    @motors = current_will.motors.where.not(:id => nil)
     @jewelry = current_will.jewelries.build
-    @jewelries = current_will.jewelries
+    @jewelries = current_will.jewelries.where.not(:id => nil)
   end
 
   def update 
