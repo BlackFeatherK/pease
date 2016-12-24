74dd6f635d11daa6831526212a02973181d09316
Brian Wong <brian.wongst@gmail.com>
2016-12-24 21:22:05 +0800

    Add user information to user Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222110658) do
+ActiveRecord::Schema.define(version: 20161224131725) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "will_id"
@@ -147,6 +147,9 @@ ActiveRecord::Schema.define(version: 20161222110658) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.date     "birthday"
+    t.string   "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
