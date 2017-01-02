c39244fc1c0e8f63827d2f6c8814f2444afcf19f
Brian Wong <brian.wongst@gmail.com>
2017-01-02 16:11:54 +0800

    adjust audio submit button

--- app/assets/javascripts/audios.js
+++ app/assets/javascripts/audios.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('.audio-choose').hide();
   $('article').hide();
   $('section').hide();
-  $('.submit').hide();
+  $('.audio-sub').hide();
   $('.message-text').hide();
 
   $('.audio_heir_true').click(function(){
@@ -19,20 +19,20 @@ $(document).ready(function(){
   });
 
   $('.audio-video').click(function(){
-    $('.submit').hide();
+    $('.audio-submit').hide();
     $('.message-text').stop().slideUp(800);
     $('section').stop().slideDown(800);
     $('article').stop().slideDown(800);
-    $('.submit').stop().slideDown(800);
+    $('.audio-submit').stop().slideDown(800);
   });
 
   $('.audio-message').click(function(){
-    $('.submit').hide();
-    $('.submit').stop().slideUp(800);
+    $('.audio-submit').hide();
+    $('.audio-submit').stop().slideUp(800);
     $('section').stop().slideUp(800);
     $('article').stop().slideUp(800);
     $('.message-text').stop().slideDown(800);
-    $('.submit').stop().slideDown(800);
+    $('.audio-submit').stop().slideDown(800);
   });
 
 });
